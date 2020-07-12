da7955db2e7a3921bc43fd7a268b7a8991b61483
AzulAqueduct <68088952+AzulAqueduct@users.noreply.github.com>
2020-07-11 20:47:49 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
+ "parserOptions": {
+  "ecmaVersion": 6,
+  "ecmaFeatures": {
+    "experimentalObjectRestSpread": true
+  }
+},
  rules: {
     'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }]
-  }
+  },
 }
