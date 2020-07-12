25637b69f3b38ed54defdaad47b48f5ce16522f7
AzulAqueduct <68088952+AzulAqueduct@users.noreply.github.com>
2020-07-11 21:18:10 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,10 @@
 module.exports = {
- "extends" : ['plugin:vue/vue3-recommended'],
+ "extends": ['plugin:vue/vue3-recommended'],
  "parser": "vue-eslint-parser",
  "parserOptions": {
     "parser": "vue-eslint-parser",
     "sourceType": "module"
-  }
-},
+  },
  rules: {
     'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }]
   },
