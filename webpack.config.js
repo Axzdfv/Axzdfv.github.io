d3098f08f5d4386b48ed8095540dd939afd0ccb7
AzulAqueduct <68088952+AzulAqueduct@users.noreply.github.com>
2020-07-11 20:34:38 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js/,
+        exclude: [ path.resolve(__dirname, 'node_modules') ],
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
         test: /\.vue$/,
         exclude: [ path.resolve(__dirname, 'node_modules') ],
         loader: 'babel-loader'
