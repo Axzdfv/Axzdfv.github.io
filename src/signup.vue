7f57e92cf0b2e96cdd5a97a91c01c6a4bc47a08e
AzulAqueduct <68088952+AzulAqueduct@users.noreply.github.com>
2020-07-22 18:10:12 -0700

    Update signup.vue

--- src/signup.vue
+++ src/signup.vue
@@ -15,67 +15,57 @@
   import "firebase/auth";
   
   export default{
-    name: "signup",
-    el: "#app",
+new Vue({
+  name: "signup",
+  el: "#app",
   data() {
     return {
-      firebaseConfig: {
-        apiKey: "AIzaSyDUqQjUOgvHp0dtaH8zTafyD7MAvlz0vlg",
-        authDomain: "rubyaetherrake.firebaseapp.com",
-        databaseURL: "https://rubyaetherrake.firebaseio.com",
-        projectId: "rubyaetherrake",
-        storageBucket: "rubyaetherrake.appspot.com",
-        messagingSenderId: "862436012917",
-        appId: "1:862436012917:web:9e8a952b02a8902b71193c",
-        measurementId: "G-YVDV8R9J80"
-      },
-      username: "",
-      password: "",
-      message: "testing 123"
-    };
+    firebaseConfig: {
+      apiKey: "AIzaSyDUqQjUOgvHp0dtaH8zTafyD7MAvlz0vlg",
+      authDomain: "rubyaetherrake.firebaseapp.com",
+      databaseURL: "https://rubyaetherrake.firebaseio.com",
+      projectId: "rubyaetherrake",
+      storageBucket: "rubyaetherrake.appspot.com",
+      messagingSenderId: "862436012917",
+      appId: "1:862436012917:web:9e8a952b02a8902b71193c",
+      measurementId: "G-YVDV8R9J80"
+    },
+    username: "",
+    password: "",
+    message: ""
+    }
   },
-  methods:{
-    signUp(){
-      firebase.initializeApp(this.firebaseConfig);
-        if (firebase.auth().currentUser) {
-        // [START signout]
-        firebase.auth().signOut();
-        console.log("signing out");
-        // [END signout]
-      } else {
-        firebase
-          .auth()
-          .createUserWithEmailAndPassword(this.username, this.password)
-          .then(function () {
-            this.message="986";
-            a();
-            // window.location.href("http://stackoverflow.com");
-          })
-          .catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // [START_EXCLUDE]
-            if (errorCode == "auth/weak-password") {
-              this.message = "The password is too weak.";
-            } else {
-              //console.log(errorMessage);
-            }
-            this.message=errorMessage;
-            // [END_EXCLUDE]
-          });
-        //window.location.replace("http://stackoverflow.com");
-      }
-      function a() {
-        firebase.auth().onAuthStateChanged((user) => {
-          if (user) {
-            console.log("9");
-            window.location.href = "http://stackoverflow.com"; //After successful signup, user will be redirected to home.html
+  created: function () {
+    firebase.initializeApp(this.firebaseConfig);
+    if (firebase.auth().currentUser){
+      this.message = "user already logged in";
+    } else {
+      this.message = "Sign up!";
+   }
+  },
+  methods: {
+    signUp() {
+      var self = this;
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(self.username, self.password)
+        .then(function () {
+          if (firebase.auth().currentUser){
+            console.log("Signup successful!");
+            self.message = "Signup successful!";
+          } else {
+            console.log("???");
           }
+      })
+        .catch(function (error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          self.message = errorMessage;
+          console.log(errorMessage);
         });
-      }
     }
   }
+});
   }
   
 </script>
