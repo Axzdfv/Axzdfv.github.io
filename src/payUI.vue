aa49e15787ad8d4b779fdf6984c6018fc92bf850
AzulAqueduct <68088952+AzulAqueduct@users.noreply.github.com>
2020-08-19 12:52:22 -0700

    Create payUI.vue

---
+++ src/payUI.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="app">
+  <div id="dropin-wrapper">
+    <div id="a"></div>
+    <div id="dropin-container"></div>
+    <button id="submit-button">Submit payment</button>
+  </div>
+  {{m}}
+  <button>rea</button>
+</div>
+</template>
+
+<script>
+  import * as firebase from "firebase/app";
+  import "firebase/auth";
+  import "firebase/firestore";
+  import axios from 'axios'
+</script>
