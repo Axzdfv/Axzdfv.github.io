cbc0122f0291b752b0bf59cd31e249e1f962a12a
AzulAqueduct <68088952+AzulAqueduct@users.noreply.github.com>
2020-08-19 13:01:05 -0700

    Update payUI.vue

--- src/payUI.vue
+++ src/payUI.vue
@@ -15,4 +15,44 @@
   import "firebase/auth";
   import "firebase/firestore";
   import axios from 'axios'
+  var dropin = require('braintree-web-drop-in');
+  
+  export default {
+    name: payui,
+    el: "#app"
+    data(){
+      return {
+        firebaseConfig: {
+          apiKey: "AIzaSyDUqQjUOgvHp0dtaH8zTafyD7MAvlz0vlg",
+          authDomain: "rubyaetherrake.firebaseapp.com",
+          databaseURL: "https://rubyaetherrake.firebaseio.com",
+          projectId: "rubyaetherrake",
+          storageBucket: "rubyaetherrake.appspot.com",
+          messagingSenderId: "862436012917",
+          appId: "1:862436012917:web:9e8a952b02a8902b71193c",
+          measurementId: "G-YVDV8R9J80"
+        },
+        db: null,
+        m: ""
+      }
+    },
+    created() {
+      firebase.initializeApp(this.firebaseConfig);
+      var functions = firebase.functions();
+      var self = this;
+      //self.db = firebase.firestore();
+      firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+          self.db=firebase.firestore();
+          var uid = firebase.auth().currentUser.uid;
+          firebase
+            .auth()
+            .currentUser.getIdToken()
+            .then(function (uidToken) {
+              self.getToken(uidToken);
+            });
+          }
+       });
+     },
+  }
 </script>
